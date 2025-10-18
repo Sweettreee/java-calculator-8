8af6cf0eae5e057786ca36f30276a0c6079af8ea
Sweettreee <xhrlehgus1234@naver.com>
2025-10-18 22:07:20 +0900

    feat: get string value

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+class Game {
+    String stringForCalculating;
+
+    void getValue() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        stringForCalculating = Console.readLine();
+    }
+
+    void run() {
+        getValue();
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        game.run();
     }
 }
