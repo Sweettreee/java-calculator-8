03ce525cf9394db08dcf9e839bfd8833d8824369
Sweettreee <xhrlehgus1234@naver.com>
2025-10-18 22:17:39 +0900

    feat: extract delimiter from string

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,14 +4,29 @@ import camp.nextstep.edu.missionutils.Console;
 
 class Game {
     String stringForCalculating;
+    String delimiter;
 
     void getValue() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         stringForCalculating = Console.readLine();
     }
 
+    void extractDelimiter() {
+        String defaultDelimiter = ",:";
+        char firstChar = stringForCalculating.charAt(0);
+
+        if (firstChar == '/') {
+            int idx = stringForCalculating.indexOf("\\n");
+            delimiter = defaultDelimiter.concat(stringForCalculating.substring(2, idx));
+            stringForCalculating = stringForCalculating.substring(idx + 2);
+        } else {
+            delimiter = defaultDelimiter;
+        }
+    }
+
     void run() {
         getValue();
+        extractDelimiter();
     }
 }
 
